71ea0fec93ffc49b6ab5d2cc89b27551103a6956
Mark Du <dulongjiebackup@gmail.com>
2024-07-16 12:11:30 -0400

    search func

--- src/lib.rs
+++ src/lib.rs
@@ -24,16 +24,26 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let content = fs::read_to_string(config.file_path)?;
-    print!("Content:\n{content}");
+    
+    for line in search(&config.query, &content){
+        println!("{line}");
+    }
     Ok(())
 
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    for line in contents.lines(){
-        
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
     }
+
+    results
 }
+
 
 
 #[cfg(test)]
